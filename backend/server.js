05cd6d0b1ff21893e3ee291be8231898780bd118
anonymousliem <muslimsyuhadah@gmail.com>
2021-03-09 16:17:35 +0700

    change port backend

--- backend/server.js
+++ backend/server.js
@@ -311,7 +311,8 @@ app.post('/uploads', async (req, res, next) => {
 /*** END MULTER ***/
 
 //Server listening
-app.listen(4000,() =>{
+var port = process.env.PORT || 4000;
+app.listen(port,() =>{
   console.log('Server started on port 4000...');
 });
 
